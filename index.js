6fd07829047ad35584e310e205e3a0e19b85dc93
Çağın Kordel <caginkordel@bilfen.com>
2022-11-23 23:02:18 +0300

    koko

--- index.js
+++ index.js
@@ -5,15 +5,38 @@ const axios = require("axios");
 const app = express();
 const port = 3000;
 
+const today = new Date();
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+app.get("/coin", async (req, res) => {
+  let datas = await getCoinData();
+  let superTrendData = SuperTrend(datas, 3, 10).reverse();
+  const lastDatas = superTrendData.slice(0, 2);
+
+  if (lastDatas.length > 1) {
+    if (lastDatas[0].trendDirection > lastDatas[1].trendDirection) {
+      console.log(" AL");
+    } else if (lastDatas[0].trendDirection < lastDatas[1].trendDirection) {
+      console.log(" SAT");
+    } else {
+      console.log(" HOLD");
+    }
+  }
+
+  res.send(lastDatas);
+});
+
 app.get("/", async (req, res) => {
   let buySymbols = "";
   let sellSymbols = "";
-  //let standSymbols = "";
+  let buy = 0;
+  let sell = 0;
   await yahooFinance.historical(
     {
-      symbols: await getData(),
-      from: "2015-01-01",
-      to: new Date().toISOString().slice(0, 10),
+      symbols: ["VESTL.IS"], //await getData(),
+      from: "2017-01-01",
+      to: tomorrow.toISOString().slice(0, 10),
       period: "d",
     },
     function (err, result) {
@@ -22,37 +45,59 @@ app.get("/", async (req, res) => {
         if (element.length > 0) {
           let superTrendData = SuperTrend(element.reverse(), 3, 10).reverse();
           const lastDatas = superTrendData.slice(0, 2);
-          if (lastDatas[0].trendDirection > lastDatas[1].trendDirection) {
-            buySymbols += "-" + element[0].symbol.replace(".IS", "") + "\r\n";
-            //console.log(element[0].symbol + " AL");
-          } else if (lastDatas[0].trendDirection < lastDatas[1].trendDirection) {
-            sellSymbols += "-" + element[0].symbol.replace(".IS", "") + "\r\n";
-            //console.log(element[0].symbol + " SAT");
-          } else {
-            //standSymbols += "- " + element[0].symbol.replace(".IS", "") + "\r\n";
-            console.log(element[0].symbol + " BEKLE");
+          if (lastDatas.length > 1) {
+            if (lastDatas[0].trendDirection > lastDatas[1].trendDirection) {
+              buySymbols += "-" + element[0].symbol.replace(".IS", "") + "\r\n";
+              buy = 1;
+              console.log(element[0].symbol + " AL");
+            } else if (lastDatas[0].trendDirection < lastDatas[1].trendDirection) {
+              sellSymbols += "-" + element[0].symbol.replace(".IS", "") + "\r\n";
+              sell = 1;
+              console.log(element[0].symbol + " SAT");
+            } else {
+              console.log(element[0].symbol + " HOLD");
+            }
           }
-        } else {
-          console.log("Data alınamadı");
         }
       });
     }
   );
 
-  if (buySymbols.length > 1 || sellSymbols.length > 1) {
-    sendSignal("ALINACAK HİSSELER:  \r\n" + buySymbols + "SATILACAK HİSSELER:  \r\n" + sellSymbols);
+  if (buy == 1 || sell == 1) {
+    console.log("AL: " + buySymbols + "SAT: " + sellSymbols);
+    //sendSignal("ALINACAK HİSSELER:  \r\n" + buySymbols + "\r\nSATILACAK HİSSELER:  \r\n" + sellSymbols + "\r\n\r\nNot: Yatırım tavsiyesi değildir 😎");
   } else {
-    sendSignal("Bugün rahat olun dostlar, bir aksiyonumuz bulunmamaktadır. Takipte kalın 😎");
+    console.log("YOK");
+    //sendSignal("Bugün rahat olun dostlar, bir aksiyonumuz bulunmamaktadır. Takipte kalın 😎");
   }
 
   res.send("OK");
 });
 
+const getCoinData = async () => {
+  let alldata = [];
+  const url = "https://api.binance.me/api/v1/klines?symbol=ETHTRY&interval=4h&limit=1000";
+  await axios
+    .get(url)
+    .then((res) => {
+      res.data.forEach((element) => {
+        alldata.push({
+          //open: element[1],
+          high: element[2] * 1,
+          low: element[3] * 1,
+          close: element[4] * 1,
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+  return alldata;
+};
+
 const getData = async () => {
   let allSymbols = [];
   const config = {
     headers: {
-      authorization: "apikey 7uttd9gDUtjxB8jjB5GSNn:2nLKrZP25W5K5iyu0waoz2",
+      authorization: "apikey 69TLINLlGOvKeH6Es8cGrC:6pIf9h7gByNjijN7kk1Vmm",
     },
   };
   const url = "https://api.collectapi.com/economy/hisseSenedi";
@@ -75,7 +120,7 @@ const sendSignal = async (text) => {
   await axios
     .get(url)
     .then((res) => {
-      console.log(res);
+      //console.log(res);
     })
     .catch((err) => console.log(err));
   return;
